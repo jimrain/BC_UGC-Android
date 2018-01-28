b993accef7aa76b0b9a07ede90a9de52a771d823
jimrain-bc <jrainville@brightcove.com>
2018-01-28 07:58:36 -0800

    Adding more to Brightcove CMS class

--- app/src/main/java/net/rainville/android/bcugc/BrightcoveCMS.java
+++ app/src/main/java/net/rainville/android/bcugc/BrightcoveCMS.java
@@ -1,5 +1,7 @@
 package net.rainville.android.bcugc;
 
+import java.net.URL;
+
 /**
  * Created by jim on 1/27/18.
  */
@@ -9,11 +11,22 @@ public class BrightcoveCMS {
     private String ClientID;
     private String ClientSecret;
 
+    private static final String OauthUrl = "https://oauth.brightcove.com/v4/access_token";
+
+
     public BrightcoveCMS() {}
 
     public BrightcoveCMS(String accountID, String clientID, String clientSecret) {
         AccountID = accountID;
         ClientID = clientID;
         ClientSecret = clientSecret;
+    }
+
+    public String getAccessToken() {
+        String accessToken = null;
+
+        // URL authEndpoint = new URL(OauthUrl);
+
+        return (accessToken);
     }
 }
